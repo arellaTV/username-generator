0bec01d480b827d30635605d34565a738ba4ff46
arellaTV <jeremiah.arella@gmail.com>
2024-04-11 15:18:14 -0700

    Configures datepicker for date of birth

--- frontend/src/Form/index.tsx
+++ frontend/src/Form/index.tsx
@@ -10,9 +10,20 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { FruitSelector } from "./components/FruitSelector";
-import { DatePicker } from "./components/DatePicker";
+import Datepicker, { DateValueType } from "react-tailwindcss-datepicker";
+import { useState } from "react";
 
 export function Form() {
+  const [value, setValue] = useState<DateValueType>({
+    startDate: new Date(),
+    endDate: null,
+  });
+
+  const handleValueChange = (newValue: DateValueType) => {
+    console.log("newValue:", newValue);
+    setValue(newValue);
+  };
+
   return (
     <Card className="w-[350px]">
       <CardHeader>
@@ -34,7 +45,14 @@ export function Form() {
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="lastName">Date of Birth</Label>
-              <DatePicker />
+              <Datepicker
+                value={value}
+                onChange={handleValueChange}
+                asSingle
+                useRange={false}
+                toggleClassName="absolute right-0 top-0 h-full px-3 text-gray-400 focus:outline-none disabled:opacity-40 disabled:cursor-not-allowed"
+                inputClassName="relative flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="framework">Fruit</Label>
