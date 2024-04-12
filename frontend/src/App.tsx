27c0a6450b2a830b11e2b92c08dd6466e8a745b8
arellaTV <jeremiah.arella@gmail.com>
2024-04-11 21:27:10 -0700

    Adds toast to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { Form } from "./Form";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <main className="w-full min-h-screen flex justify-center items-center">
       <Form />
+      <Toaster />
     </main>
   );
 }
